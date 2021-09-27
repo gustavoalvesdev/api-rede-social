f97131b18c70d9620444b91f19a6be3add5553e7
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2021-09-27 12:29:56 -0300

    dando like no post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Post;
+use App\PostLike;
 
 class PostController extends Controller
 {
@@ -12,5 +14,48 @@ class PostController extends Controller
         $this->middleware("auth:api");
 
         $this->loggedUser = auth()->user();
+    }
+
+    public function like($id)
+    {
+        $array = ['error' => ''];
+
+        // 1. Verificar se o post existe
+        if (Post::find($id)) {
+            // 2. Verificar se eu já dei like no post
+            $isLiked = PostLike::where('id_post', $id)
+            ->where('id_user', $this->loggedUser['id'])
+            ->count();
+
+            if ($isLiked > 0) {
+                // 2.1 Se sim, remover
+                $pl = PostLike::where('id_post', $id)
+                ->where('id_user', $this->loggedUser['id'])
+                ->first();
+
+                $pl->delete();
+
+                $array['isLiked'] = false;
+            } else {
+                // 2.2 Se não, adicionar
+                $newPostLike = new PostLike();
+                $newPostLike->id_post = $id;
+                $newPostLike->id_user = $this->loggedUser['id'];
+                $newPostLike->created_at = date('Y-m-d H:i:s');
+                $newPostLike->save();
+
+                $array['isLiked'] = true;
+            }
+
+            $array['likeCount'] = PostLike::where('id_post', $id)->count();
+
+
+        } else {
+            $array['error'] = 'Post não existe!';
+            return $array;
+        }
+
+
+        return $array;
     }
 }
